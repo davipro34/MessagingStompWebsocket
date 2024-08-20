035966f3feb2ef408cdab695f0612e47a1813dbe
David <david@davipro.fr>
2024-08-20 10:31:58 +0200

    Add initial project files and configurations

---
+++ src/main/java/fr/davipro/messagingstompwebsocket/MessagingstompwebsocketApplication.java
@@ -0,0 +1,13 @@
+package fr.davipro.messagingstompwebsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagingstompwebsocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessagingstompwebsocketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/davipro/messagingstompwebsocket/MessagingstompwebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.davipro.messagingstompwebsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagingstompwebsocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
